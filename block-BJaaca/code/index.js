20cbf4483c453157162ea6311b5f23853f08908d
Devanand kumar <deva.zebu288@gmail.com>
2022-12-01 17:18:40 +0530

    Assignment: Accessing and Assigning

--- block-BJaaca/code/index.js
+++ block-BJaaca/code/index.js
@@ -2,23 +2,51 @@
 
 //  - Create a variable named `character` using `let` assign a value of empty object.
 
+```Js
+let character = {};
+
+```
+
 //  - Go to this [Arya Stark](https://awoiaf.westeros.org/index.php/Arya_Stark)
 
 //  Re-assign the value of `character` variable to and object with the key `characterName` and value of "Arya"
+```Js
+character = {
+    charecterName : "Arya";
+}
+
+```
 
 // - A variable named `age` is predefined with value 20. Add a new key named `character-age` with the value of `age` variable i.e 20 (don't use the value 20)
 
 let age = 20;
+charecter = ["charector-age"] = age;
 
 // - Add a new key `sur name` with the value of the surname of the character i.e "Stark". (Don't remove the space between `sur` and `name`)
 
+charecter = ["sur name"] = "Stark";
+
 // - Add a new key `title` with the value title of the character i.e "Lady of Winterfell"
+
+charecter.title = "Lady of Winterfell";
 
 // - Add another property named `greet` and value should be a function when called should alert `I am [NAME HERE] and my title is [TITLE HERE]`.
 
+charecter.greet = `I am ${charecter.charecterName} and my title is ${charecter.title}`
+charecter.greet = function () {
+    alert(
+        `I am ${charecter.charecterName} and my title is ${charecter.title}`
+    );
+}
+
 // - Add a new property to the character object named `isFemale` and value will be `true`
 
+charecter.idFemale = true;
+
 // - Execute the function stored in the greet key of the character object
+
+charecter.greet;
+charecter.greet();
 
 //  - Change the method `greet` to now alert `She is [NAME HERE] and her title is [TITLE HERE]`.
 
